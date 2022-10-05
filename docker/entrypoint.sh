8fe69697e4e8d95561671e65f4cda5984a85c746
Ted Sanders <tpsjr7@gmail.com>
2022-10-04 23:08:54 -0500

    update docker, add cmake, and updates conda if the environment.yaml changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd ~/app
+set -euxo pipefail
+cd /app
 
 if [ "$HUGGINGFACE_TOKEN" = "" ] ||  [ "$HUGGINGFACE_TOKEN" = "your_token_here" ] ; then
   echo "Missing HUGGINGFACE_TOKEN, visit https://huggingface.co/settings/tokens and put it into the stablediffusion-infinity/.env file."
@@ -14,15 +15,25 @@ set -x
 
 git submodule init
 git submodule update
-
+git config --global credential.helper store
 if ! conda env list | grep sd-inf ; then
-    git config --global credential.helper store
-    echo "Creating environment, wait a few minutes..."
+    echo "Creating environment, it may appear to freeze for a few minutes..."
     conda env create -f environment.yml
+    echo "Finished installing."
     echo "conda activate sd-inf" >> ~/.bashrc
+    shasum environment.yml > ~/.environment.sha
 fi
 
 . "/opt/conda/etc/profile.d/conda.sh"
 conda activate sd-inf
 
-python app.py --port=8888
+if shasum -c ~/.environment.sha > /dev/null 2>&1 ; then
+  echo "environment.yml is unchanged."
+else
+  echo "environment.yml was changed, please wait a minute until it says 'Done updating'..."
+  conda env update --file environment.yml
+  shasum environment.yml > ~/.environment.sha
+  echo "Done updating."
+fi
+
+python app.py --port=8888 --host=0.0.0.0
